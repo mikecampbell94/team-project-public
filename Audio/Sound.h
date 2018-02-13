26d4282993884d6106c2f6e7b48e27cb11f9a079
b4065648 <b4065648@TERRAN29>
2018-02-13 12:59:13 +0000

    Created Sound class for encapsulating sound data and soundsource to be able to play sounds in game

---
+++ Audio/Sound.h
@@ -0,0 +1,55 @@
+#pragma once
+
+//#pragma comment(lib, "Resource Management.lib")
+
+#include "OpenAL 1.1 SDK\include\al.h"
+#include "..\Resource Management\Resources\Resource.h"
+
+#include <string>
+#include <map>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+typedef unsigned long DWORD;
+
+struct FMTCHUNK 
+{
+	short format;
+	short channels;
+	DWORD srate;
+	DWORD bps;
+	short balign;
+	short samp;
+};
+
+class Sound : public Resource
+{
+	friend class SoundManager;
+public:
+
+	Sound();
+	~Sound();
+
+	char* getData() { return data; }
+	int getBitRate() { return bitRate; }
+	float getFrequency() { return freqRate; }
+	int getChannels() { return channels; }
+	int getSize() { return size; }
+	ALuint getBuffer() { return buffer; }
+	ALenum getOALFormat();
+	double getLength();
+
+	//void loadFromWAV(string filename);
+	//void loadWAVChunkInfo(ifstream &file, string &name, unsigned int &size);
+
+private:
+	ALuint buffer;
+	char *data;
+	float freqRate;
+	double length;
+	unsigned int bitRate;
+	unsigned int size;
+	unsigned int channels;
+};
---
+++ Audio/SoundSource.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include "../Utilities/Maths/Vector3.h"
+#include "Sound.h"
+
+#include <algorithm>
+
+enum SoundPriority 
+{
+	SOUNDPRIORTY_LOW,
+	SOUNDPRIORITY_MEDIUM,
+	SOUNDPRIORITY_HIGH,
+	SOUNDPRIORITY_ALWAYS
+};
+
+struct OALSource
+{
+	ALuint source;
+	bool inUse;
+	
+	OALSource(ALuint src) 
+	{
+		source = src;
+		inUse = false;	}
+};
+
+class SoundSource
+{
+public:
+	SoundSource();
+	SoundSource(Sound *s, Vector3 position, float volume);
+	~SoundSource();
+
+	void reset();
+
+	void setSound(Sound *s);
+	Sound* getSound() { return sound; }
+
+	void setPriority(SoundPriority p) { priority = p; }
+	SoundPriority getPriority() { return priority; }
+
+	void setVolume(float volume) { volume = min(1.0f, max(0.0f, volume)); }
+	float getVolume() { return volume; }
+
+	void setLooping(bool state) { isLooping = state; }
+	bool getLooping() { return isLooping; }
+
+	void setRadius(float value) { radius = max(0.0f, value); }
+	float getRadius() { return radius; }
+
+	float getPitch() { return pitch; }
+	void setPitch(float value) { pitch = value; }
+
+	bool getIsGlobal() { return isGlobal; }
+	void setIsGlobal(bool value) { isGlobal = value; }
+
+	double getTimeLeft() { return timeLeft; }
+
+	OALSource* getSource() { return oalSource; }
+
+	static bool compareSourcesByPriority(SoundSource *a, SoundSource *b);
+
+	void attachSource(OALSource* s);
+	void detachSource();
+
+	void update(float msec);
+
+private:
+	Sound* sound;
+	Vector3 position;
+	OALSource* oalSource;
+	SoundPriority priority;
+	float volume;
+	float radius;
+	float pitch;
+	bool isLooping;
+	bool isGlobal;
+	double timeLeft;
+};
+
